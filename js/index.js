59bf4cb7f23f2f4f48ad4c447fb52e208adba97b
Brandon Luna Castro <blunacastro2003@gmail.com>
2023-07-27 17:35:38 -0300

    fixed: fix the main draw function
    BREAKING CHANGE: add input type range and refactor the main function draw ,for draw the board with  the values of the input range width and height

--- js/index.js
+++ js/index.js
@@ -3,41 +3,38 @@ let container = document.querySelector(".container")
 
 
 const draw = (dimension) => {
-    let grid = 664/dimension;
-    console.log(grid)
-    let div = document.createElement("div")
-    div.classList.add("div");
-    document.querySelector('.div')
-    /* clase.style.width = grid,'px'
-    clase.style.height = grid,'px'
-
-    container.appendChild(div)
- */
-    console.log(document.querySelector(div))
-/*  
+    
+    let grid = 483/dimension;
+    let fragment = document.createDocumentFragment();
 
     for(let i = 1;i <= dimension;i++){
         let index = 1 
 
         while(index <= dimension){
             
-                
-                container.appendChild(div);
-                index++;
-
-            }
+              let div = document.createElement("div")
+    
+              div.classList.add("square")
+              fragment.appendChild(div) 
+              
                
-                
-            
-            
+                index++;
+            }
            
-           } */
-     
+           } 
+           let clone = document.importNode(fragment)
+           container.appendChild(fragment)
+        drawSquare(".square",grid)  
 } 
 
-
-
-
+const drawSquare = (clases,grid) =>{
+    let nodo = document.querySelectorAll(clases)
+    nodo.forEach((el)=>{
+         el.style.width = `${grid}px`
+         el.style.height = `${grid}px`
+     
+    })
+} 
 
 let range = document.querySelector("#range"),
     rangeValue = document.querySelector(".range-value")
@@ -47,9 +44,12 @@ let range = document.querySelector("#range"),
     range.addEventListener("input",e => {
   
         rangeValue.innerText = range.value 
+        container.innerText = ""
         draw(range.value)
 
 })
+
+ window.addEventListener("DOMContentLoaded",draw(5))
 
 /* let btns = document.querySelectorAll("[data-longitud]")
 console.log(btns)
