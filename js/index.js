5ed91648ce4739ffbe5332b271ee5759eb517693
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-07-28 10:35:47 -0300

    fixed: add events and functions for paint squares
    BREAKING CHANGE: add mouse events for painting squares at press click

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-let container = document.querySelector(".container")
-,tamanio = 16;
-
+let container = document.querySelector(".container");
 
 const draw = (dimension) => {
     
@@ -9,53 +7,73 @@ const draw = (dimension) => {
 
     for(let i = 1;i <= dimension;i++){
         let index = 1 
-
-        while(index <= dimension){
-            
-              let div = document.createElement("div")
-    
-              div.classList.add("square")
-              fragment.appendChild(div) 
-              index++;
-            }
-        } 
-           let clone = document.importNode(fragment)
-           container.appendChild(fragment)
+        while(index <= dimension){          
+          let div = document.createElement("div");
+          div.classList.add("square");
+          fragment.appendChild(div) ;
+          index++;
+        }
+    } 
+     
+        container.appendChild(fragment);
         drawSquare(".square",grid)  
 } 
 
 const drawSquare = (clases,grid) =>{
-    let nodo = document.querySelectorAll(clases)
+    let nodo = document.querySelectorAll(clases);
     nodo.forEach((el)=>{
-         el.style.width = `${grid}px`
-         el.style.height = `${grid}px`
-     
+         el.style.width = `${grid}px`;
+         el.style.height = `${grid}px`;
     })
 } 
 
+// codigo para el input de rango
 let range = document.querySelector("#range"),
-    rangeValue = document.querySelector(".range-value")
+    rangeValue = document.querySelector(".range-value");
 
-    rangeValue.innerText = range.value
+    rangeValue.innerText = range.value;
 
     range.addEventListener("input",e => {
   
-        rangeValue.innerText = range.value 
-        container.innerText = ""
-        draw(range.value)
+        rangeValue.innerText = range.value ;
+        container.innerText = "";
+        draw(range.value);
 
 })
 
- window.addEventListener("DOMContentLoaded",draw(8))
+/* Selecionamos el color del input color*/
 
-/* let btns = document.querySelectorAll("[data-longitud]")
-console.log(btns)
-    btns.forEach((btn)=>{
-        btn.addEventListener("click",e => {
-            tamanio = Number(e.target.getAttribute("data-longitud"))
-            container.innerText = ""
-            draw(tamanio)
-        })
+let color = document.getElementById("input-color"),
+    colorSelected = color.value
 
-    })
- */
+color.addEventListener('change',e => {
+    colorSelected = e.target.value; 
+})
+
+
+/*Seccion de eventos para pintar*/
+
+const paintSquare = (e) => {
+    console.log(e.target)
+    e.target.style.backgroundColor = colorSelected
+    console.log(colorSelected)
+}
+
+const mousePosition = (e) => {
+    console.log("entra a mouse Position")
+    e.target.style.backgroundColor = colorSelected
+    container.addEventListener("mouseover",paintSquare)
+}
+
+const removeEvent = () => {
+    container.removeEventListener('mouseover',paintSquare)
+}
+
+//cuando hacemos click en un square se muestra mientras mantengamos presionado el boton sino se remueve el evento
+container.addEventListener("mousedown", mousePosition)
+container.addEventListener("mouseup",removeEvent)
+
+window.addEventListener("DOMContentLoaded",e => {
+    draw(8);
+
+});
