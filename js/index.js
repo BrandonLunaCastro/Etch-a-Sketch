914cb4df811decb828140b1b631cf954725af57e
Brandon Luna Castro <blunacastro2003@gmail.com>
2023-07-28 20:30:27 -0300

    fixed: add button 'random color' , and styles basis elements
    BREAKING CHANGE: add  button 'random color' functionality for painting squares if select this input

--- js/index.js
+++ js/index.js
@@ -10,6 +10,7 @@ const draw = (dimension) => {
         let index = 1 
         while(index <= dimension){          
           let div = document.createElement("div");
+          div.setAttribute("data-dark","100")
           div.classList.add("square");
           fragment.appendChild(div) ;
           index++;
@@ -31,6 +32,10 @@ const drawSquare = (clases,grid) =>{
     })
 } 
 
+//funcion de los colores random
+
+const randomRgb = () => Math.round(Math.random() * (255 - 0 + 1) + 0 ).toFixed(0);    
+
 // codigo para el input de rango
 let range = document.querySelector("#range"),
     rangeValue = document.querySelector(".range-value");
@@ -47,11 +52,11 @@ let range = document.querySelector("#range"),
 
 /* Selecionamos el color del input color*/
 
-let color = document.getElementById("input-color"),
-    colorSelected = color.value
+let color = document.getElementById("input-color")
+  
 
 color.addEventListener('change',e => {
-    colorSelected = e.target.value; 
+    localStorage.setItem("color",e.target.value)
 })
 
 /* Seccion de botones */
@@ -59,47 +64,74 @@ let btns = document.querySelector(".buttons")
 console.log(btns)
  
 btns.addEventListener("click",e => {
-    if(e.target.matches(".grid-lines")){
-         let squares = document.querySelectorAll(".square");
+    let squares = document.querySelectorAll(".square");
+    
+    //boton para limpiar la pizarra
+    if(e.target.matches(".reset")){
+        squares.forEach((square) =>{
+            square.style.backgroundColor = "#fefefe"
+        })
+        localStorage.removeItem("opacity-mode")
+    }
+    //boton para eliminar el local storagede 'random mode'
+    if(e.target.matches(".color-mode")){
+        localStorage.removeItem("randomMode");
+        localStorage.setItem("color",color.value);
+    }
+
+    //Boton para usar la funcion de colores aleatorios
+    if(e.target.matches(".random-mode")){
+        localStorage.setItem("randomMode",true)
+        localStorage.setItem("color",`rgb(${randomRgb()},${randomRgb()},${randomRgb()})`)
+    }
+
+    //boton para hacer uso de la funcion de opacidad
+    if(e.target.matches(".opacity-mode")){
+        localStorage.setItem("opacity-mode","active")     
+    }
+
+    //boton grid-lines
+    if(e.target.matches(".grid-lines")){   
          squares.forEach((square) => {
-             square.classList.toggle("active-border");   
-        
+             square.classList.toggle("active-border");         
             if(square.className.includes("active-border")){
                  localStorage.setItem("border",true);
             }else{
-                localStorage.setItem("border",false);
+                 localStorage.setItem("border",false);
         }
     })
     }
 
 
 })
- 
-
 /*Seccion de eventos para pintar*/
 
 const paintSquare = (e) => {
-    console.log(e.target)
-    e.target.style.backgroundColor = colorSelected
-    console.log(colorSelected)
+    localStorage.getItem("randomMode") === "true" 
+        ? e.target.style.backgroundColor = `rgb(${randomRgb()},${randomRgb()},${randomRgb()})`
+        : e.target.style.backgroundColor = localStorage.getItem("color");
+            
+        
+        /*  localStorage.getItem("opacity-mode") === "active" 
+            ? e.target.style.filter = `brightness(${Number(e.target.dataset.dark) - 10}%)`  */
 }
 
 const mousePosition = (e) => {
     console.log("entra a mouse Position")
-    e.target.style.backgroundColor = colorSelected
-    container.addEventListener("mouseover",paintSquare)
+    e.target.style.backgroundColor = localStorage.getItem("color");
+    container.addEventListener("mouseover",paintSquare);
 }
 
 const removeEvent = () => {
-    container.removeEventListener('mouseover',paintSquare)
+    container.removeEventListener('mouseover',paintSquare);
 }
 
 //cuando hacemos click en un square se muestra mientras mantengamos presionado el boton sino se remueve el evento
-container.addEventListener("mousedown", mousePosition)
-container.addEventListener("mouseup",removeEvent)
+container.addEventListener("mousedown", mousePosition);
+container.addEventListener("mouseup",removeEvent);
 
 window.addEventListener("DOMContentLoaded",e => {
     draw(8);
-   
+    localStorage.setItem("color",color.value)
 
 });
